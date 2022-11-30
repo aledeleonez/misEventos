67e68c14be9b8502d334648909125a6121b59df3
Alejandro de León Fernández <aledeleonez@gmail.com>
2022-11-30 11:00:19 +0100

    activities creadas

---
+++ app/src/main/java/com/example/miseventos/activity_nuevoEvento.java
@@ -0,0 +1,14 @@
+package com.example.miseventos;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class activity_nuevoEvento extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nuevo_evento);
+    }
+}
